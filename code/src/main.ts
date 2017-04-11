b8b749b4f63ab4f71a7a552576b5e2d7a9311f58
Vincent den Boer <vincent@shishkabab.net>
2017-04-11 11:12:13 +0200

    Update Typscript and start main server

--- code/src/main.ts
+++ code/src/main.ts
@@ -1,10 +1,24 @@
-require('source-map-support').install();
+require('source-map-support').install()
+import { MemoryProfileStorage } from './profile-storage'
+import { SingleCodeGenerator } from './code-generator'
+import { MemoryConfirmationSender } from './confirmation-sender'
+import { MemoryVerificationStorage } from './verification-storage'
+import { MemoryClaimsStorage } from './claims-storage'
+import { EmailVerifier } from './email-verifier'
+import { createServer } from './server'
 
 
 const DEVELOPMENT_MODE = process.env.NODE_ENV === 'dev';
 
 
 function main(){
+    createServer({emailVerifier: new EmailVerifier({
+        claims: new MemoryClaimsStorage(),
+        verification: new MemoryVerificationStorage(),
+        confirmationSender: new MemoryConfirmationSender(),
+        codeGenerator: new SingleCodeGenerator('1234'),
+        profile: new MemoryProfileStorage({testUser: 'testContract'})
+    })}).listen(4567)
 }
 
 
