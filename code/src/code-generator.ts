7c82a8209de46a1779db5d11b46cd3bbce27c558
HamedSaidi <hamedessaidi@gmail.com>
2017-05-08 12:24:46 +0200

    random code generator for phone and email verification

--- code/src/code-generator.ts
+++ code/src/code-generator.ts
@@ -15,12 +15,33 @@ export class SingleCodeGenerator implements CodeGenerator {
 
 export class RandomCodeGenerator implements CodeGenerator {
   private codeLength : number
-
-  constructor({codeLength} : {codeLength : number}) {
+  private randomCode : string
+  constructor({codeLength, digitOnly} : {codeLength : number, digitOnly : boolean}) {
     this.codeLength = codeLength
+    if (digitOnly) {
+      const max = this.calculateMax(codeLength)
+      const min = this.calculateMin(codeLength)
+      this.randomCode = (Math.floor(Math.random() * (max - min)) + min).toString()
+    } else {
+      this.randomCode = randtoken.generate(codeLength)
+    }
+  }
+
+  calculateMax(codeLength : number) : number {
+    if (codeLength === 0) return 0
+    let max = 1
+    for (let i = 0; i < codeLength; i++) max *= 10
+    return max - 1
+  }
+
+  calculateMin(codeLength : number) : number {
+    if (codeLength < 1) return 0
+    let min = 1
+    for (let i = 0; i < codeLength - 1; i++) min *= 10
+    return min
   }
 
   generateCode() : string {
-    return randtoken.generate(16)
+    return this.randomCode
   }
 }
