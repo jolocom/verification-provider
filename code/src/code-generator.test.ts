6e4e191a3d79336ed3da9cf3df0cecaf56655c2e
HamedSaidi <hamedessaidi@gmail.com>
2017-05-09 15:34:13 +0200

    update generate random code and unit tests

--- code/src/code-generator.test.ts
+++ code/src/code-generator.test.ts
@@ -3,7 +3,35 @@ import { RandomCodeGenerator } from './code-generator';
 
 describe('RandomCodeGenerator', () => {
   it('should be able to generate random codes of a given length', async () => {
-    const generator = new RandomCodeGenerator({codeLength: 16})
+    let generator = new RandomCodeGenerator({codeLength: 6, digitOnly: true})
+    expect(generator.generateCode()).to.have.lengthOf(6)
+    generator = new RandomCodeGenerator({codeLength: 16, digitOnly: false})
     expect(generator.generateCode()).to.have.lengthOf(16)
+
+
+  })
+  it('should generate a new code different form the previvious one', () => {
+    const generator = new RandomCodeGenerator({codeLength: 16, digitOnly: false})
+    expect(generator.generateCode()).to.not.equal(generator.generateCode())
+    expect(generator.generateCode()).to.not.equal(generator.generateCode())
+    expect(generator.generateCode()).to.not.equal(generator.generateCode())
+    expect(generator.generateCode()).to.not.equal(generator.generateCode())
+  })
+  it('should generate a numeric code if digitOnly is true', () => {
+    const generator = new RandomCodeGenerator({codeLength: 16, digitOnly: true})
+    expect(/^[0-9]*$/.test(generator.generateCode())).to.be.true
+    expect(/^[0-9]*$/.test(generator.generateCode())).to.be.true
+    expect(/^[0-9]*$/.test(generator.generateCode())).to.be.true
+    expect(/^[0-9]*$/.test(generator.generateCode())).to.be.true
+    expect(/^[0-9]*$/.test(generator.generateCode())).to.be.true
+    expect(/^[0-9]*$/.test(generator.generateCode())).to.be.true
+  })
+  it('should generate a random alphanumeric code', () => {
+    const generator = new RandomCodeGenerator({codeLength: 16, digitOnly: false})
+    expect(/^[0-9]*$/.test(generator.generateCode())).to.be.false
+    expect(/^[a-z]*$/.test(generator.generateCode())).to.be.false
+    expect(/^[A-Z]*$/.test(generator.generateCode())).to.be.false
+    expect(/^[a-z0-9]+$/i.test(generator.generateCode())).to.be.true
+
   })
 })
--- code/src/code-generator.ts
+++ code/src/code-generator.ts
@@ -15,20 +15,17 @@ export class SingleCodeGenerator implements CodeGenerator {
 
 export class RandomCodeGenerator implements CodeGenerator {
   private codeLength : number
-  private randomCode : string
+  private digitOnly : boolean
+
   constructor({codeLength, digitOnly} : {codeLength : number, digitOnly : boolean}) {
     this.codeLength = codeLength
-    if (digitOnly) {
-      const Random = require("random-js")
-      const max = (codeLength === 0) ? 0 : (10 ** codeLength) - 1
-      const min = (codeLength <= 1) ? 0 : 10 ** (codeLength - 1)
-      this.randomCode = Random(Random.engines.mt19937().autoSeed()).integer(min, max).toString()
-    } else {
-      this.randomCode = randtoken.generate(codeLength)
-    }
+    this.digitOnly = digitOnly
   }
 
   generateCode() : string {
-    return this.randomCode
+      const elements = this.digitOnly ? '0123456789'
+        : '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      return randtoken.generate(this.codeLength, elements)
+
   }
 }
