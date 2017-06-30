30fd96ce648ce949c4728ccf1a086c787e951a6e
Vincent den Boer <vincent@shishkabab.net>
2017-06-30 17:48:37 +0200

    Check for payments when adding verifying claims

--- code/src/claims-storage.ts
+++ code/src/claims-storage.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash'
 import * as contracts from 'smartwallet-contracts'
 import { AttributeType } from './types'
 
@@ -7,7 +8,7 @@ interface ClaimStoreReturnValue {
 
 export interface ClaimsStorage {
   verifyAttribute(params : {
-    contractID : string, attrType : AttributeType, value : string
+    txHash : string, salt : string, attrType : AttributeType, value : string
   }) : Promise<ClaimStoreReturnValue>
 }
 
@@ -15,7 +16,7 @@ export class MemoryClaimsStorage implements ClaimsStorage {
   public claims = []
 
   async verifyAttribute(params : {
-    contractID : string, attrType : AttributeType, value : string
+    txHash : string, salt : string, attrType : AttributeType, value : string
   }) : Promise<ClaimStoreReturnValue> {
     this.claims.push(params)
     return {
@@ -47,20 +48,64 @@ export class EthereumClaimStorage implements ClaimsStorage {
   }
 
   async verifyAttribute(params : {
-    contractID : string, attrType : AttributeType, value : string
+    txHash : string, salt : string, attrType : AttributeType, value : string
   }) : Promise<ClaimStoreReturnValue> {
     const definitionUrl = {
       email: '',
       phone: ''
     }[params.attrType]
 
-    await this._wallet.addAttributeHashAndWait(
-      params.attrType,
-      params.value,
-      definitionUrl,
-      this._config.password,
-      params.contractID
+    const transaction = this._wallet.getTransaction(params.txHash)
+    const walletAddress = transaction.from
+    const identityAddress = this._wallet.getIdentityAddressFromLookupContract(walletAddress)
+    const calculatedVerificationHash = this._wallet.walletCrypto.sha256(
+      params.value + params.salt
     )
+    if (transaction.data !== calculatedVerificationHash) {
+      throw new Error("Hash does not match")
+    }
+
+    const existingHash = await this._wallet.getAttributeHash({
+      attributeId: params.attrType,
+      identityAddress
+    })
+
+    let forceAddVerification = false
+    if (!existingHash) {
+      forceAddVerification = true
+
+      await this._wallet.addAttributeHashToTargetIdentityAndWait({
+        targetIdentityAddress: identityAddress,
+        attributeId: params.attrType,
+        attribute: params.value,
+        definitionUrl: '',
+        pin: this._config.password,
+      })
+    }
+
+    if (!forceAddVerification) {
+      const verificationCount = await this._wallet.getNumberOfVerifications({
+        attributeId: params.attrType,
+        identityAddress
+      })
+      const verifications = await Promise.all(_.range(verificationCount).map(idx =>
+        this._wallet.getVerification({
+          attributeId: params.attrType,
+          verificationIdx: idx,
+          identityAddress
+        })
+      ))
+      const calculatedHash = this._wallet.walletCrypto.calculateDataHash(params.value)
+      const exists = _.some(verifications,
+        verification => verification.hash === calculatedHash
+      )
+    }
+
+    await this._wallet.addVerificationToTargetIdentity({
+      targetIdentityAddress: identityAddress,
+      attributeId: params.attrType,
+      pin: this._config.password,
+    })
 
     return {claimID: 'dummy'}
   }
